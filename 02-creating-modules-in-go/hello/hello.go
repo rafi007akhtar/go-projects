2464aeb8561705a65e3d6d5c22014171711ae506
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-11-02 16:30:00 +0530

    modified greetings and hello go files to handle errors for empty name

--- 02-creating-modules-in-go/hello/hello.go
+++ 02-creating-modules-in-go/hello/hello.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	var msg = greetings.Hello("Rafi")
+	var msg, err = greetings.Hello("Rafi")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(msg)
 }
