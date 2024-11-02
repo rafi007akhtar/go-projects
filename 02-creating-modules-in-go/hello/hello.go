f959310511a021ed8713a99b0dce74142f200df4
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-11-02 17:08:53 +0530

    added logger settings

--- 02-creating-modules-in-go/hello/hello.go
+++ 02-creating-modules-in-go/hello/hello.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	// logger settings
+	log.SetPrefix("greetings: ") // set prefix
+	log.SetFlags(0)              // don't log timestamp and source file info
+
 	var msg, err = greetings.Hello("Rafi")
 	if err != nil {
 		log.Fatal(err)
