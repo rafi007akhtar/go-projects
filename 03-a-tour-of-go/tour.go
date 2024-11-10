8c01b2ae7398a50b78bb2f3e9e1e4d6b5069acbe
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-11-10 18:47:17 +0530

    added generics file execution as a choice in the main tour file

--- 03-a-tour-of-go/tour.go
+++ 03-a-tour-of-go/tour.go
@@ -5,14 +5,20 @@ import "fmt"
 func main() {
 	var choice int
 
-	println("Enter: \n1 for basics, \n2 for methods and interpreters \nYour choice:")
+	println("Enter: \n1 for basics, \n2 for methods and interpreters \n3 for generics \nYour choice:")
 	fmt.Scanf("%d\n", &choice)
+	if choice != 1 && choice != 2 {
+		choice = 3
+		println("We are going with the default choice, which is", choice)
+	}
 
 	switch choice {
 	case 1:
 		TourBasics()
 	case 2:
 		TourMethodsAndInterfaces()
+	case 3:
+		TourGenerics()
 	default:
 		println("This choice is not valid")
 	}
