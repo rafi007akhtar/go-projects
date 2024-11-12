cbc6de87eb3fd9d445fa414ba610882069e10953
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-11-12 23:32:27 +0530

    started with channels

--- 03-a-tour-of-go/tour-concurrency.go
+++ 03-a-tour-of-go/tour-concurrency.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func say(word string, n int) {
 	for i := 0; i < n; i++ {
@@ -9,8 +12,39 @@ func say(word string, n int) {
 	}
 }
 
+/*
+Input a slice of ints and a channel
+Sum the slice and send to the channel
+*/
+func sumCh(nums []int, ch chan int) {
+	var sum = 0
+	for _, num := range nums {
+		sum += num
+	}
+	ch <- sum // NOTE: this is where we send the value to the channel
+}
+
 func TourConcurrency() {
 	// 01 - goroutines
 	go say("hello", 5)
 	say("world", 5)
+
+	// 02 - channels
+	// from GFG website: In Go language,
+	// a channel is a medium through which a goroutine communicates with another goroutine
+	// and this communication is lock-free.
+	// Task: split an slice in two, sum each half in a channel, print both the values and their sum
+	var nums = []int{7, 2, 8, -9, 4, 0}
+	var firstHalf = nums[:len(nums)/2]
+	var secondHalf = nums[len(nums)/2:]
+	// now allocate a channel, and create two goroutines where the channel will be involved
+	var ch = make(chan int)
+	go sumCh(firstHalf, ch)
+	go sumCh(secondHalf, ch)
+	// now extract the values put into these channels into variables
+	var (
+		x = <-ch
+		y = <-ch
+	)
+	fmt.Printf("Extracted values: %v, %v; their sum: %v\n", x, y, x+y)
 }
